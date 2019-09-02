93ffaa4fdd438db74752f72d6b43bef48249da8c
weikaisen <weikaisen@zy.com>
2019-09-02 15:04:38 +0800

    初始化

---
+++ src/main/java/com/xianzhi/platform/XianzhiPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.xianzhi.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XianzhiPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XianzhiPlatformApplication.class, args);
+    }
+
+}
